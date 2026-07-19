06fc943f82e0ca1408884e8446c5d223592a3f91
Muhammed <oloyedemuhammed12@gmail.com>
2026-07-19 16:26:00 +0100

    ft:design theme setup

---
+++ frontend/src/theme/typography.ts
@@ -0,0 +1,21 @@
+import { Inter, Montserrat, Poppins } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+  weight: ["400", "500", "600", "700"],
+});
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+  display: "swap",
+});
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  display: "swap",
+  weight: ["400", "500", "600", "700"],
+});
