d6266b93f1e163986da8fb2f8744bef37d6c9e24
Charles Ugberaese <ugberaeseac@gmail.com>
2026-07-24 20:18:58 +0100

    fix Opportunity field

--- backend/apps/opportunities/admin.py
+++ backend/apps/opportunities/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Opportunity
+from .models import Opportunity, OpportunityField
 
 admin.site.register(Opportunity)
+admin.site.register(OpportunityField)
