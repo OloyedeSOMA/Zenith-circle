bbceeb42088a1bee47943ef8c1c9f96e67731b14
Muhammed <oloyedemuhammed12@gmail.com>
2026-08-14 17:21:37 +0100

    merge

--- frontend/src/app/recruiter-dashboard/layout.tsx
+++ frontend/src/app/recruiter-dashboard/layout.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { Home, LayoutDashboard, Bookmark, Clock, Bell, User, Menu,} from "lucide-react";
+import { Home, LayoutDashboard, Bookmark, FilePlus2, ClipboardList, User, Menu,} from "lucide-react";
 import Sidebar, { type SidebarNavLink } from "@/components/dashboard-components/Sidebar";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import { Suspense } from "react";
@@ -21,12 +21,12 @@ const recruiterNavLinks: SidebarNavLink[] = [
     {
       label: "Create Opportunities",
       href: "/recruiter-dashboard/create-opportunity",
-      icon: Bookmark,
+      icon: FilePlus2,
     },
     {
       label: "Submissions",
       href: "/recruiter-dashboard/submissions",
-      icon: Bookmark,
+      icon: ClipboardList,
     },
     {
       label: "Profile",
